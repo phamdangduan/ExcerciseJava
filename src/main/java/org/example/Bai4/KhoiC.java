6dac4051a0dd657c269509f3ca188a53b3fd2360
Pham Dang Duan <dangduan210605@gmail.com>
2025-09-18 09:36:13 +0700

    hoan thanh bai 4

---
+++ src/main/java/org/example/Bai4/KhoiC.java
@@ -0,0 +1,18 @@
+package org.example.Bai4;
+
+public class KhoiC extends KhoiA {
+	// thuoc tinh
+
+	// phuong thuc
+	// ham khoi tao
+	public KhoiC() {
+
+	}
+
+	public KhoiC(String mon1, String mon2, String mon3) {
+		super(mon1, mon2, mon3);
+	}
+}
+
+
+
