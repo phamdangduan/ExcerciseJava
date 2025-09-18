9ab825254d1addfa74d6537d20005975e023e61f
Pham Dang Duan <dangduan210605@gmail.com>
2025-09-18 09:01:03 +0700

    hoan thanh bai 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,34 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.Bai1.PhanSo;
+
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc=new Scanner(System.in);
+        PhanSo ps1=new PhanSo();
+        PhanSo ps2=new PhanSo();
+        PhanSo psTong =new PhanSo();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // nhap phan so
+        System.out.println("Nhap vao phan so thu nhat:");
+        ps1.nhapPS(sc);
+        System.out.println("Nhap vao phan so thu hai:");
+        ps2.nhapPS(sc);
+
+        // tinh tong 2 phan so
+        psTong=ps1.congPS(ps2);
+
+        // hien thi phan so
+        System.out.println("\nPhan so thu nhat la:");
+        ps1.hienThiPS();
+        System.out.println("\nPhan so thu hai la:");
+        ps2.hienThiPS();
+        System.out.println("\nPhan so tong la:");
+        psTong.hienThiPS();
+
+        sc.close();
     }
 }
