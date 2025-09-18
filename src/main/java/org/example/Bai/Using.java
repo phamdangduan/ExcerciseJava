2a08d806b848794b53ac4cd4e000729fb21f0c6c
Pham Dang Duan <dangduan210605@gmail.com>
2025-09-18 10:04:54 +0700

    hoan thanh bai 10

---
+++ src/main/java/org/example/Bai/Using.java
@@ -0,0 +1,25 @@
+package org.example.Bai;
+
+import org.example.Bai10.QuanLy;
+
+import java.util.Scanner;
+
+
+
+public class Using {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		QuanLy ql = new QuanLy();
+		ql.nhapDanhSach(sc);
+
+		System.out.println("Danh sach bien lai cua cac ho gia dinh la:");
+		ql.hienThiDanhSach();
+
+		sc.close();
+	}
+
+}
+
+
