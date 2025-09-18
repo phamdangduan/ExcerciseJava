e2fd48eb62cdd191d7abc7e5180dd233b6ffcf73
Pham Dang Duan <dangduan210605@gmail.com>
2025-09-18 09:40:20 +0700

    hoan thanh bai 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
+import org.example.Bai5.KhuPho;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        KhuPho qlkp = new KhuPho();
+
+        qlkp.nhapDanhSach(sc);
+
+        System.out.println("Danh sach ho dan co nguoi 80 tuoi la:");
+        qlkp.timKiemThongTin();
     }
 }
